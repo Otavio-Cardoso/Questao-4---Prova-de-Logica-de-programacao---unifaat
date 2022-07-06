153e2637403237c1621c41456943eeaff58171ad
Otávio Cardoso <101899470+Otavio-Cardoso@users.noreply.github.com>
2022-07-06 19:55:39 -0300

    Update Program.cs

--- Questao 4 - Prova de de Logica de progamacao - unifaat/Program.cs
+++ Questao 4 - Prova de de Logica de progamacao - unifaat/Program.cs
@@ -10,6 +10,8 @@ namespace Questao_4___Prova_de_de_Logica_de_progamacao___unifaat
     {
         static void Main(string[] args)
         {
+            //------------- ALUNO : OTÁVIO CARDOSO RA 6322528 -------------
+            
             int cont;
             Console.WriteLine("Programa para realizar a contagem de 1 a 50.\n Aperte qualquer tecla para continuar...");
             Console.ReadKey();
